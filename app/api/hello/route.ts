139f60c0a54db46d5c0113f2a815f181e5c82a46
Pedro Boas <pedrokvb@gmail.com>
2024-01-20 01:45:44 -0300

    chore: test api

---
+++ app/api/hello/route.ts
@@ -0,0 +1,15 @@
+export const runtime = 'edge'
+
+export async function GET(req: Request) {
+  // return stream response (SSE)
+  return new Response('Hello World', {
+    headers: new Headers({
+      // since we don't use browser's EventSource interface, specifying content-type is optional.
+      // the eventsource-parser library can handle the stream response as SSE, as long as the data format complies with SSE:
+      // https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#sending_events_from_the_server
+
+      // 'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+    }),
+  })
+}
