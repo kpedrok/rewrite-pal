7c666e1c1de9bdff95b54ec16b218bb9e0fc6d1d
Pedro Boas <pedrokvb@gmail.com>
2024-01-19 22:11:42 -0300

    style: add animations

--- app/page.tsx
+++ app/page.tsx
@@ -16,13 +16,13 @@ export default function Home() {
   const [isGPT, setIsGPT] = useState(true)
 
   return (
-    <div className='flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen'>
+    <div className='flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen '>
       <Header />
-      <main className='flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20'>
-        <h1 className='sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900 mb-5'>
+      <main className='flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20 animate-in fade-in duration-1000'>
+        <h1 className='sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900 hover:scale-105 transition duration-300 ease-in-out'>
           Write your text and let me check your grammar
         </h1>
-        <p className='border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out'>
+        <p className='border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out  mt-7 '>
           <b>112</b> phrases improved so far
         </p>
         {/* <div className='mt-7'>{<Toggle isGPT={isGPT} setIsGPT={setIsGPT} />}</div> */}
