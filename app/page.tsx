97909f3e835a7943d6a0f6767cef696cb7b63c2e
Pedro Boas <pedrokvb@gmail.com>
2024-01-19 21:12:48 -0300

    chore: update github star button

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       <Header />
       <main className='flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20'>
         <p className='border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out'>
-          <b>96,434</b> phrases improved so far
+          <b>112</b> phrases improved so far
         </p>
         <h1 className='sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900'>
           Write your text and let me check your grammar
--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
       </Link>
       <a
         className='flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100'
-        href='https://github.com/Nutlope/twitterbio'
+        href='https://github.com/kpedrok/rewrite-ai'
         target='_blank'
         rel='noopener noreferrer'>
         <Github />
