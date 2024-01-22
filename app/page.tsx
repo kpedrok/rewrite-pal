76ccbbc2444212c651c6bd7c3906010a84506a3b
Pedro Boas <pedrokvb@gmail.com>
2024-01-21 22:56:08 -0300

    chore: update number icon

--- app/page.tsx
+++ app/page.tsx
@@ -127,7 +127,7 @@ export default function Home() {
 
         <div className='max-w-xl w-full'>
           <div className='flex mt-10 items-center space-x-3'>
-            <NumberOne weight='light' size={30} color='#ffffff' alt='1 icon' className=' bg-black rounded-full p-1' />
+            <NumberOne weight='regular' size={30} color='#ffffff' alt='1 icon' className=' bg-black rounded-full p-1' />
             <p className='text-left font-medium'>
               Paste your text here
               <span className='text-slate-500'> (⌘+V)</span>.
@@ -142,7 +142,7 @@ export default function Home() {
             placeholder={'Type or paste your text here.'}
           />
           <div className='flex mb-5 items-center space-x-3'>
-            <NumberTwo weight='light' size={30} color='#ffffff' alt='2 icon' className=' bg-black rounded-full p-1' />{' '}
+            <NumberTwo weight='regular' size={30} color='#ffffff' alt='2 icon' className=' bg-black rounded-full p-1' />{' '}
             <p className='text-left font-medium'>Select your tone.</p>
           </div>
           <div className='block'>
