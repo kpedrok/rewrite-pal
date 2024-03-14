43d05f2c4799bb92b09565c71c7eb7cc51479ccf
Pedro Boas <pedrokvb@gmail.com>
2024-03-14 17:11:05 -0300

    chore: add email

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,40 +1,35 @@
-import { GithubLogo, InstagramLogo } from '@phosphor-icons/react/dist/ssr'
+import { Envelope, GithubLogo } from '@phosphor-icons/react/dist/ssr'
 import Link from 'next/link'
 
 export default function Footer() {
   return (
     <footer className='text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3'>
       <div>
-        Powered by{' '}
+        Give feedback{' '}
         <a
-          href='https://openai.com/'
+          href='mailto:hi@rewriteai.xyz'
           target='_blank'
           className='font-bold hover:underline transition underline-offset-2'>
-          OpenAI{' '}
+          hi@rewriteai.xyz{' '}
         </a>
-        and{' '}
+        {/* and{' '}
         <a
           href='https://vercel.com/'
           target='_blank'
           className='font-bold hover:underline transition underline-offset-2'>
           Vercel
-        </a>
+        </a> */}
       </div>
       <div className='flex space-x-4 pb-4 sm:pb-0'>
-        <Link href='https://github.com/kpedrok' className='group' target='_blank' aria-label='TaxPal on GitHub'>
+        <Link href='https://github.com/kpedrok' className='group' target='_blank' aria-label='Rewrite AI on GitHub'>
           <GithubLogo weight='bold' aria-hidden='true' className='h-6 w-6 fill-slate-500 group-hover:fill-slate-700' />
         </Link>
-        <Link
-          href='https://www.instagram.com/kpedrok'
-          target='_blank'
-          className='group'
-          aria-label='TaxPal on Instagram'>
-          <InstagramLogo
-            weight='bold'
-            aria-hidden='true'
-            className='h-6 w-6 fill-slate-500 group-hover:fill-slate-700'
-          />
+        <Link href='mailto:hi@rewriteai.xyz' target='_blank' className='group' aria-label='Rewrite AI Email'>
+          <Envelope weight='bold' aria-hidden='true' className='h-6 w-6 fill-slate-500 group-hover:fill-slate-700' />
         </Link>
+        {/* <Link href='mailto:hi@rewriteai.xyz' target='_blank' className='group' aria-label='Rewrite AI Email'>
+          hi@rewriteai.xyz
+        </Link> */}
       </div>
     </footer>
   )
