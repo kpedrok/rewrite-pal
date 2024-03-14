1cbe4634c22b9c98bdfcedc2f7947f23abab5948
Pedro Boas <pedrokvb@gmail.com>
2024-03-14 16:52:49 -0300

    chore: add sitemap and crawler

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://rewriteai.vercel.app/',
+      url: 'https://rewriteai.xyz/',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
