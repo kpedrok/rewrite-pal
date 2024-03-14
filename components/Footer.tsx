ee1550393444ff028869f6da8941800abd348229
Pedro Boas <pedrokvb@gmail.com>
2024-03-14 19:47:38 -0300

    chore: update copy on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className='text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3'>
       <div>
-        Give feedback{' '}
+        Share your feedback with me at{' '}
         <a
           href='mailto:hi@rewriteai.xyz'
           target='_blank'
