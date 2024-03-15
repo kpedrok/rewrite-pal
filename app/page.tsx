00c3f9c58e83c04fb43499e33ba3e9a292ad7072
Pedro Boas <pedrokvb@gmail.com>
2024-03-15 18:22:03 -0300

    chore: update number format

--- app/page.tsx
+++ app/page.tsx
@@ -174,7 +174,7 @@ export default function Home() {
           Instantly generate clear, compelling writing while maintaining your unique voice.
         </h3>
         <p className='border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out  mt-7 '>
-          <b>{views}</b> phrases improved so far
+          <b>{views.toLocaleString()}</b> phrases improved so far
         </p>
         {/* <div className='mt-7'>{<Toggle isGPT={isGPT} setIsGPT={setIsGPT} />}</div> */}
         <div className='max-w-4xl w-full'>
@@ -215,12 +215,12 @@ export default function Home() {
             ))}
           </ToggleGroup>
 
-          <div className='flex mb-5 mt-5 items-center space-x-3'>
+          <div className='flex mb-5 mt-6 items-center space-x-3'>
             <NumberThree
               weight='regular'
               size={30}
               color='#ffffff'
-              alt='2 icon'
+              alt='3 icon'
               className=' bg-black rounded-full p-1'
             />{' '}
             <p className='text-left font-medium'>Language: </p>
