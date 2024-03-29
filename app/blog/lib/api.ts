4367063e5ae7304040c7cd699dfb1acf78a95f85
Pedro Boas <pedrokvb@gmail.com>
2024-03-29 13:36:44 -0300

    refact: update file names

--- app/blog/lib/api.ts
+++ app/blog/lib/api.ts
@@ -1,28 +1,28 @@
-import { Post } from "@/interfaces/post";
-import fs from "fs";
-import matter from "gray-matter";
-import { join } from "path";
+import fs from 'fs'
+import matter from 'gray-matter'
+import { join } from 'path'
+import { Post } from '../interfaces/post'
 
-const postsDirectory = join(process.cwd(), "_posts");
+const postsDirectory = join(process.cwd(), '_posts')
 
 export function getPostSlugs() {
-  return fs.readdirSync(postsDirectory);
+  return fs.readdirSync(postsDirectory)
 }
 
 export function getPostBySlug(slug: string) {
-  const realSlug = slug.replace(/\.md$/, "");
-  const fullPath = join(postsDirectory, `${realSlug}.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
-  const { data, content } = matter(fileContents);
+  const realSlug = slug.replace(/\.md$/, '')
+  const fullPath = join(postsDirectory, `${realSlug}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const { data, content } = matter(fileContents)
 
-  return { ...data, slug: realSlug, content } as Post;
+  return { ...data, slug: realSlug, content } as Post
 }
 
 export function getAllPosts(): Post[] {
-  const slugs = getPostSlugs();
+  const slugs = getPostSlugs()
   const posts = slugs
     .map((slug) => getPostBySlug(slug))
     // sort posts by date in descending order
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
-  return posts;
+    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
+  return posts
 }
