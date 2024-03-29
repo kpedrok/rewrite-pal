e6282dfa0d567275a534467dea81cec773c85a04
Pedro Boas <pedrokvb@gmail.com>
2024-03-29 13:41:51 -0300

    build: fix imports

--- app/blog/_components/post-preview.tsx
+++ app/blog/_components/post-preview.tsx
@@ -1,5 +1,5 @@
-import { type Author } from '@/interfaces/author'
 import Link from 'next/link'
+import { Author } from '../interfaces/author'
 import Avatar from './avatar'
 import CoverImage from './cover-image'
 import DateFormatter from './date-formatter'
