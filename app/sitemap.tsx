f0d6149e4f1ce6f9ccde0e6798d69b5ab691538a
Pedro Boas <pedrokvb@gmail.com>
2024-04-01 12:41:14 -0300

    chore: fix sitemap

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.rewriteai.xyz/',
+      url: 'https://www.rewriteai.xyz',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
