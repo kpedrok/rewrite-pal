23e7782c753c05803461625de5b6c32130a3b6c5
Pedro Boas <pedrokvb@gmail.com>
2024-04-20 15:09:12 -0300

    ci: redeploy

--- apps/rewriter/app/page.tsx
+++ apps/rewriter/app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
   const [loading, setLoading] = useState(false)
   const [bio, setBio] = useState('')
   const [generatedBios, setGeneratedBios] = useState<String>('')
+
   const resultRef = useRef<null | HTMLDivElement>(null)
 
   const [selectedVibes, setSelectedVibes] = useState<string[] | undefined>(undefined)
