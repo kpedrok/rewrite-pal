67b0c9f99b66de87edd624cec6b21430163aff8f
Pedro Boas <pedrokvb@gmail.com>
2024-04-20 15:36:25 -0300

    ci: turbo run build --filter=apps/rewriter

--- apps/rewriter/app/page.tsx
+++ apps/rewriter/app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
   const [bio, setBio] = useState('')
   const [generatedBios, setGeneratedBios] = useState<String>('')
   const resultRef = useRef<null | HTMLDivElement>(null)
+
   const [selectedVibes, setSelectedVibes] = useState<string[] | undefined>(undefined)
 
   useEffect(() => {
