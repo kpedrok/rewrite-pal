6a9d1d853bbcd1c68ae2e9f029d20bdc44c9c15d
Pedro Boas <pedrokvb@gmail.com>
2024-04-22 11:33:32 -0300

    chore: update roadmap logo

---
+++ packages/ui/src/logo.tsx
@@ -0,0 +1,75 @@
+export function RewritePalLogo(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    // <svg aria-label='Vercel Logo' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 19' >
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 256' aria-label='RewritePal Logo' {...props}>
+      <rect width='256' height='256' fill='none' />
+      <rect
+        x='24'
+        y='56'
+        width='208'
+        height='144'
+        rx='8'
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='12'
+      />
+      <line
+        x1='56'
+        y1='128'
+        x2='200'
+        y2='128'
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='12'
+      />
+      <line
+        x1='56'
+        y1='96'
+        x2='200'
+        y2='96'
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='12'
+      />
+      <line
+        x1='56'
+        y1='160'
+        x2='64'
+        y2='160'
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='12'
+      />
+      <line
+        x1='96'
+        y1='160'
+        x2='160'
+        y2='160'
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='12'
+      />
+      <line
+        x1='192'
+        y1='160'
+        x2='200'
+        y2='160'
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='12'
+      />
+    </svg>
+  )
+}
