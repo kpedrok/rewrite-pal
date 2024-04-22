eb54edaabe6d3f67b95528a4ce42bdc04f0502f4
Pedro Boas <pedrokvb@gmail.com>
2024-04-22 00:16:45 -0300

    add docs and tailwind ui

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -1,15 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 // We want each package to be responsible for its own content.
-const config: Omit<Config, "content"> = {
+const config: Omit<Config, 'content'> = {
   theme: {
     extend: {
       backgroundImage: {
-        "glow-conic":
-          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
+        'glow-conic': 'conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
