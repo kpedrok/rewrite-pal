c28f22d824dae6bd08175d6e17116e2f19cb0eb9
Pedro Boas <pedrokvb@gmail.com>
2024-04-23 09:49:41 -0300

    remove eos dots

--- apps/rewriter/app/page.tsx
+++ apps/rewriter/app/page.tsx
@@ -195,7 +195,7 @@ export default function Home() {
             <NumberOne weight='regular' size={30} color='#ffffff' alt='1 icon' className=' bg-black rounded-full p-1' />
             <p className='text-left font-medium'>
               Paste your text here
-              <span className='text-slate-500'> (⌘+V)</span>.
+              <span className='text-slate-500'> (⌘+V)</span>
             </p>
           </div>
           <textarea
@@ -210,7 +210,7 @@ export default function Home() {
           <div className='flex mb-5 items-center space-x-3'>
             <NumberTwo weight='regular' size={30} color='#ffffff' alt='2 icon' className=' bg-black rounded-full p-1' />{' '}
             <p className='text-left font-medium'>
-              Select your tone <span className='text-slate-500'> (optional)</span>.
+              Select your tone <span className='text-slate-500'> (optional)</span>
             </p>
           </div>
 
