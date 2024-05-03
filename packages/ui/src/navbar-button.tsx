e08d60389a32422588242f6b3a19079297a65760
Pedro Boas <pedrokvb@gmail.com>
2024-05-03 20:07:54 -0300

    (ui) rounded navbar button

--- packages/ui/src/navbar-button.tsx
+++ packages/ui/src/navbar-button.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react'
 export function NavbarButton({ children }: { children: ReactNode }, className?: string) {
   return (
     <button
-      className={`max-w-fit h-full items-center justify-center space-x-2 px-4 py-2 text-sm text-gray-600 transition-colors hover:bg-gray-100 hover:rounded-md  ${className || ''}`}>
+      className={`max-w-fit h-full items-center justify-center space-x-2 px-4 py-2 text-sm text-gray-600 transition-colors hover:bg-gray-100 rounded-md  ${className || ''}`}>
       {children}
     </button>
   )
